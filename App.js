87a1ddcc8c4e43627dd0f103960d97a7230942d2
Bruno_MH <brunomh141a@gmail.com>
2025-08-27 22:11:20 -0600

    BM-2ndCommit-added matrix of button numbers

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    alert("boton pressed");
+  }
+
+  const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to calculator App!</Text>
+      {matrix.map((row, rowIndex) => (
+        <View key={rowIndex} style={styles.row}>
+          {row.map((element, colIndex) => (
+            <View key={colIndex} style={styles.cell}>
+              <Pressable onPress={handlePress} style={styles.button}>
+                <Text style={styles.number}>{element}</Text>
+              </Pressable>
+            </View>
+          ))}
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +32,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    padding: 20,
+    margin: 5,
+    backgroundColor: 'gray',
+    borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
+    width: 70,
+    height: 70,
   },
+  number: {
+    color: 'white',
+    fontSize: 25
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  }
 });
